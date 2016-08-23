a6bac524d8479cfc42e6c44c53bc1e8e83d3321e
Pipe <pipernene@gmail.com>
2016-08-23 16:27:46 +0200

    gh

--- piped-cookies/bookmarklet_loader.js
+++ piped-cookies/bookmarklet_loader.js
@@ -2,8 +2,9 @@ javascript:(
   function () {
     var js = document.createElement('script');
     js.setAttribute('type', 'text/javascript');
-    js.setAttribute('id', 'pipedCookiesScript');
+    //js.setAttribute('id', 'pipedCookiesScript');
     js.setAttribute('src', 'https://raw.githubusercontent.com/pipe01/piped-cookies/master/main.js');
     document.head.appendChild(js);
+    //document.on
  }()
 );
