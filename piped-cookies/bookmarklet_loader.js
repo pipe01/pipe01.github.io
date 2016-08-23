b6e7505a61917eb537d053f8b6d05b81357a554c
Pipe <pipernene@gmail.com>
2016-08-23 16:23:09 +0200

    Added piped-cookies

---
+++ piped-cookies/bookmarklet_loader.js
@@ -0,0 +1,9 @@
+javascript:(
+  function () {
+    var js = document.createElement('script');
+    js.setAttribute('type', 'text/javascript');
+    js.setAttribute('id', 'pipedCookiesScript');
+    js.setAttribute('src', 'https://raw.githubusercontent.com/pipe01/piped-cookies/master/main.js');
+    document.head.appendChild(js);
+ }()
+);
