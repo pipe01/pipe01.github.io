eb4ea38a5d241f56f12c4c139fc73036adb08c73
Pipe <pipernene@gmail.com>
2016-08-23 17:40:05 +0200

    jg

--- piped-cookies/bookmarklet_loader.js
+++ piped-cookies/bookmarklet_loader.js
@@ -7,6 +7,6 @@ javascript:{
     var jquery = document.createElement('script');
   	jquery.setAttribute('type', 'text/javascript');
   	jquery.setAttribute('src', 'https://code.jquery.com/jquery-3.1.0.min.js');
-  	jquery.onload = function() {pcInit();};
+  	jquery.onload = function() { var interval = setInterval(function() { clearInterval(interval); pcInit(); }, 12) };
 	document.head.appendChild(jquery);
 }
